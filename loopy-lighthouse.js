a175818c46428d913472f1263bfe5eae10bdcbb9
Vanessa Mugoa <vmugoa@live.ca>
2023-01-13 14:36:01 -0500

    Use a for loop to print numbers from 100 to 200 to the console, with three exceptions

--- loopy-lighthouse.js
+++ loopy-lighthouse.js
@@ -1,11 +1,14 @@
 /*
-CHALLENGE:
+LOOPY LIGHTHOUSE CHALLENGE:
 We will be writing a program that prints the numbers from 100 to 200 to the console, with three exceptions:
 
 - If the number is a multiple of 3, print the string "Loopy" instead of the number.
 - If the number is a multiple of 4, print the string "Lighthouse" instead of the number.
 - If the number is a multiple of both 3 and 4, print the string "LoopyLighthouse" instead of the number.
 - By print, we are referring to console.log.
+
+write code below!
+
 */
 
 
@@ -24,5 +27,3 @@ else {
 }
 x = x + 1 ; 
 }
-
-
